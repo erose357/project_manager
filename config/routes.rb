234c0d8f78caf93c8363fccfc9dc5f0006449da9
erose357 <erose357@gmail.com>
2017-11-05 09:03:14 -0700

    Adds user functionality to create a new project

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   get '/auth/:provider/callback', to: 'sessions#create'
 
-  resources :projects, only: [:index, :show]
+  resources :projects, only: [:index, :new, :create, :show]
 end
--- spec/features/user_creates_a_new_project_spec.rb
+++ spec/features/user_creates_a_new_project_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe "Authenticated user" do
   scenario "creates a new project" do
     user = create(:user)
-#As an authenticated user
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-#when I visit my dashboard page
+
     visit "/projects"
-#and I click on 'new project'
+
     click_on 'New Project'
-#I am redirected to the projects new page
-    expect(current_path).to be('/projects/new')
-#And I enter a project name
+
+    expect(current_path).to eq('/projects/new')
+
     fill_in "Name", with: "Tenant Improvement"
     fill_in "Street", with: "1234 Big Street"
     fill_in "City", with: "Old Victory"
     fill_in "State", with: "CO"
     fill_in "Zip", with: 12345
-#And I click 'create'
+
     click_on 'Create Project'
-#I am redirected to my dashboard
+
     expect(current_path).to eq('/projects')
-#And I see the new project in my project list
+
     expect(page).to have_link("Tenant Improvement")
   end
 end
