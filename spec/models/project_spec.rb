07e6370190e3ecafc7efb340cb703e7a9bc21071
erose357 <erose357@gmail.com>
2017-11-05 09:46:07 -0700

    Adds validations for user model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Project, type: :model do
     it { is_expected.to validate_presence_of(:name) }
   end
 
-  describe "Relationships" do
+  describe "Associations" do
     it { is_expected.to have_many(:users) }
   end
 end
