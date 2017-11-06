80c6bbe3e79e60dd9f1ba0e3857e10b1a1d53a44
erose357 <erose357@gmail.com>
2017-11-05 17:57:43 -0700

    Working on styling for the project show page, have some temporary methods in project.rb that will need cleaned up

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,16 @@ class Project < ApplicationRecord
   has_many :users, through: :user_projects
 
   validates_presence_of :name
+
+  def address
+    "#{street}, #{city}, #{state} #{zip}"
+  end
+
+  def original_sf 
+    @original_sf || 0
+  end
+
+  def revised_sf
+    @revised_sq || 0
+  end
 end
---
+++ spec/features/projects/user_sees_project_info_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Projects show page" do
+  let (:user) { create(:user) }
+  let (:projects) { create_list(:project, 3) }
+
+  scenario "user sees project info header on show page" do
+    user.projects << projects
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/projects/#{projects[1].id}"
+
+    expect(page).to have_content("Project Description")
+    expect(page).to have_content(projects[1].name)
+    expect(page).to have_content(projects[1].address)
+    expect(page).to have_content("Square Footage to be Built/Renovated:")
+    expect(page).to have_content("Square Footage for Parking/Landscaping")
+    expect(page).to have_content("Category")
+    expect(page).to have_content("Original Budget")
+    expect(page).to have_content("Revised Budget")
+    expect(page).to have_content("Estimated Costs")
+    expect(page).to have_content("Bid Award")
+    expect(page).to have_content("Bids Expected")
+    expect(page).to have_content("Change Order")
+    expect(page).to have_content("Total")
+    expect(page).to have_content("Remaining Budget")
+    expect(page).to have_content("Requisitined Costs")
+    expect(page).to have_content("Invoiced to Date")
+    expect(page).to have_content("Balance Due")
+    #category, original budget, revised budget, estimated costs(bid award, bids expected, change order, total), remaining budget, requisitioned costs(invoiced to date, balance due)
+  end
+end
+
