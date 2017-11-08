5fec4ed0df4fa38b0a072ccdf56d4575753b8c61
erose357 <erose357@gmail.com>
2017-11-07 22:13:36 -0700

    Adds vcr to project and feature test with api call

--- spec/features/weather/user_sees_10_day_forecast_spec.rb
+++ spec/features/weather/user_sees_10_day_forecast_spec.rb
@@ -6,22 +6,24 @@ RSpec.describe "Authenticated User" do
   let (:project) { create(:project) }
 
   scenario "sees 10 day forecast" do
-    user.projects << project
+    VCR.use_cassette("weather") do
+      user.projects << project
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit "projects/#{project.id}"
+      visit "projects/#{project.id}"
 
-    expect(current_path).to eq(project_path(project.id))
+      expect(current_path).to eq(project_path(project.id))
 
-    click_on "Weather"
+      click_on "Weather"
 
-    expect(current_path).to eq(project_weather_index_path(project.id))
-    expect(page).to have_css(".weekday")
-    expect(page).to have_css(".city")
-    expect(page).to have_css(".date")
-    expect(page).to have_css(".high")
-    expect(page).to have_css(".low")
-    expect(page).to have_css(".conditions")
+      expect(current_path).to eq(project_weather_index_path(project.id))
+      expect(page).to have_css(".weekday")
+      expect(page).to have_css(".city")
+      expect(page).to have_css(".date")
+      expect(page).to have_css(".high")
+      expect(page).to have_css(".low")
+      expect(page).to have_css(".conditions")
+    end
   end
 end
