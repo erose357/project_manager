e023d1f5e1bf530a019c5f07f91681634a1cbd8f
erose357 <erose357@gmail.com>
2017-11-07 18:06:04 -0700

    Adds link to weather page in projects show

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
 
   resources :projects, only: [:index, :new, :create, :show]
+  resources :weather, only: [:index]
 end
